1702d2139023660cbec309a891046900e8f80b9e
dheeraj-codebase <dheerajputtapaka0201@gmail.com>
2024-03-19 17:13:46 -0400

    streamlit code

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+# Set page title
+st.title("License Plate Detection Application")
+
+# Upload image function
+def upload_image():
+    uploaded_file = st.file_uploader("Upload Image to extract the license plates", type=["jpg", "png", "jpeg"])
+    if uploaded_file is not None:
+        with st.spinner("Processing..."):
+            # Save the uploaded image to a local file
+            with open("uploaded_image.jpg", "wb") as f:
+                f.write(uploaded_file.getvalue())
+            st.success("Image uploaded and stored successfully!")
+
+# Upload video function
+def upload_video():
+    uploaded_file = st.file_uploader("Upload Video to extract the license plates", type=["mp4"])
+    if uploaded_file is not None:
+        with st.spinner("Processing..."):
+            # Save the uploaded video to a local file
+            with open("uploaded_video.mp4", "wb") as f:
+                f.write(uploaded_file.getvalue())
+            st.success("Video uploaded and stored successfully!")
+
+# Call upload functions
+upload_image()
+upload_video()
+
+
+if st.button("Start Detection"):
+    st.success("Detection started..!")
